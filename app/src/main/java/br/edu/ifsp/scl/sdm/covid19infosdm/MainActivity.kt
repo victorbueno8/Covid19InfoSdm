792bd5247651f389a2dbdd4c25cfce25417995ec
Victor Bueno <vihto8@gmail.com>
2020-04-15 08:41:55 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/covid19infosdm/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.edu.ifsp.scl.sdm.covid19infosdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onRetrieveClick(view: View) {
+
+    }
+}
